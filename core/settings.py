1f094ee2d34f81a71a091bb889c198d824b3136a
Abinash Sinha <detonatorx764@gmail.com>
2024-07-13 10:43:37 +0530

    added allowed host

--- core/settings.py
+++ core/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-(1&vq-3v=)gppw+ylofw)kw71h&1^t-#f8x=$2u&kphp-ebjcx
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["nirmaan.onrender.com", "127.0.0.1:8000"]
 
 
 # Application definition
